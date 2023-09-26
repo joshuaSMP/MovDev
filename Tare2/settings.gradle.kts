d3dd0530adcd58752333ad301b85f0c3f52126d8
Joshua Montaño <joshuasmp_2001@ciencias.unam.mx>
2023-09-25 20:18:31 -0600

    First commit:Base of the project

---
+++ Tare2/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Tare2"
+include(":app")
+ 
